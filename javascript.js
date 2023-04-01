218154a884cfd08af91a8b6e7421e2fb24db1599
Grzegorzp1979 <pociechagrzegorz@gmail.com>
2023-04-01 21:13:48 +0200

    C° and F° added

--- javascript.js
+++ javascript.js
@@ -4,13 +4,13 @@ let resultCelsiusEl = document.getElementById("fahrenheit_result-el")
 function convertToFahrenheit() {
     let celsius = document.getElementById("celsius-el").value
     let result = (parseFloat(celsius) * 9 / 5) + 32
-    resultFahrenheitEl.innerHTML =  result.toFixed(2)   
+    resultFahrenheitEl.innerHTML =  result.toFixed(2) + "F°"  
 }
 
 function convertToCelsius() {
     let fahrenheit = document.getElementById("fahrenheit-el").value
     let result = (parseFloat(fahrenheit) - 32) * 5 / 9
-    resultCelsiusEl.innerHTML = result.toFixed(2)
+    resultCelsiusEl.innerHTML = result.toFixed(2) + "C°"
 }
 
 
