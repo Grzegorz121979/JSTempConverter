8bdfe585aaaf3a4774e938931f4b5b0d565d88b5
Grzegorzp1979 <pociechagrzegorz@gmail.com>
2023-04-01 21:07:22 +0200

    round result

--- javascript.js
+++ javascript.js
@@ -3,14 +3,14 @@ let resultCelsiusEl = document.getElementById("fahrenheit_result-el")
 
 function convertToFahrenheit() {
     let celsius = document.getElementById("celsius-el").value
-    let result = (parseInt(celsius) * 9 / 5) + 32
-    resultFahrenheitEl.innerHTML =  result   
+    let result = (parseFloat(celsius) * 9 / 5) + 32
+    resultFahrenheitEl.innerHTML =  result.toFixed(2)   
 }
 
 function convertToCelsius() {
     let fahrenheit = document.getElementById("fahrenheit-el").value
-    let result = (parseInt(fahrenheit) - 32) * 5 / 9
-    resultCelsiusEl.innerHTML = result
+    let result = (parseFloat(fahrenheit) - 32) * 5 / 9
+    resultCelsiusEl.innerHTML = result.toFixed(2)
 }
 
 
