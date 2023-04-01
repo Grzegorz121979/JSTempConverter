e2c4e7f4e8f48e3cd43690040a212ea72d7e7552
Grzegorzp1979 <pociechagrzegorz@gmail.com>
2023-04-01 20:48:13 +0200

    new div element plus new function

--- javascript.js
+++ javascript.js
@@ -1,13 +1,16 @@
-let resultEl = document.getElementById("celsius_result-el")
+let resultFahrenheitEl = document.getElementById("celsius_result-el")
+let resultCelsiusEl = document.getElementById("fahrenheit_result-el")
 
 function convertToFahrenheit() {
     let celsius = document.getElementById("celsius-el").value
     let result = (parseInt(celsius) * 9 / 5) + 32
-    resultEl.innerHTML =  result   
+    resultFahrenheitEl.innerHTML =  result   
 }
 
 function convertToCelsius() {
-
+    let fahrenheit = document.getElementById("fahrenheit-el").value
+    let result = (parseInt(fahrenheit) + 32) * 5 / 9
+    resultCelsiusEl.innerHTML = result
 }
 
 
